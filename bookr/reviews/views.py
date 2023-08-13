edc1b59bf807902a62e515ea949b4e18d2c8a538
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-08-13 20:15:55 +0200

    Created a site welcome screen

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -4,5 +4,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    name = request.GET.get("name") or "world"
-    return HttpResponse("Hello, {}!".format(name))
+    # name = request.GET.get("name") or "world"
+    # return HttpResponse("Hello, {}!".format(name))
+    # name = "world"
+    # return render(request, "base.html", {"name": name})
+    return render(request, "base.html")
