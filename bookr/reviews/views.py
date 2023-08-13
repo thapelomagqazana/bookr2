6b2beffd935ed28599e0104d25a4b619ef2f5adc
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-08-13 08:06:35 +0200

    Explored GET values and QueryDict objects

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    name = request.GET.get("name") or "world"
+    return HttpResponse("Hello, {}!".format(name))
