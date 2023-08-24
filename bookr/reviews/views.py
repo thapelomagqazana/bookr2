39469a097a5c1f27b714af730c5c0196f4b482a1
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-08-24 13:17:41 +0200

    Implemented a simple function-based view

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
 
 
 # Create your views here.
@@ -15,3 +16,9 @@ def book_search(request):
     search_text = request.GET.get("search", "")
     return render(request, "search-results.html", {"search_text": search_text})
 
+
+def welcome_view(request):
+    message = f"<html><h1>Welcome to Bookr!</h1> " \
+              f"<p>{Book.objects.count()} books and " \
+              f"counting!</p></html>"
+    return HttpResponse(message)
