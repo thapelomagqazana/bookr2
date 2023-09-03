1e3e1a4a6e72cab67aac069b184fa1bd938e62cd
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-09-04 00:01:51 +0200

    Styled and integrated the publisher form

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -96,5 +96,5 @@ def publisher_edit(request, pk=None):
     else:
         form = PublisherForm(instance=publisher)
 
-    return render(request, "form-example.html",
-                  {"method": request.method, "form": form})
+    return render(request, "reviews/instance-form.html",
+                  {"instance": publisher, "model_type": "Publisher", "form": form})
